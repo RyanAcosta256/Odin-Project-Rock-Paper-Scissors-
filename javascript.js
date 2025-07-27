0ebe4560ea3472417904d89d7f363598afa2ec3c
RyanAcosta256 <134816152+RyanAcosta256@users.noreply.github.com>
2025-07-27 02:52:16 -0400

    I've added some text to the html file

--- javascript.js
+++ javascript.js
@@ -1,10 +1,29 @@
 //console.log("Hello World");
 
 function computerChoice(){
-    const choices = ["rock", "paper", "sissors"];
+    const choices = ["rock", "paper", "scissors"];
     let index =  Math.floor(Math.random() * 3);
     return choices[index];
     
 }
+let playerChoice;
+console.log(computerChoice()); //test
 
-//console.log(computerChoice()); //test
+
+
+function getHumanChoice(){
+    
+    const choiceButton = document.querySelector("#choiceButton");
+
+    choiceButton.addEventListener("click",() => {
+        let sign = prompt("choose: rock, papper, or scissors");
+        
+
+
+
+
+
+
+    });
+
+}
