2472d4643376723a99880aad83e5c16446845cb7
Ryan <ryanacosta265@gmail.com>
2025-07-28 23:13:58 -0400

    finished adding the human choices

--- javascript.js
+++ javascript.js
@@ -6,12 +6,25 @@ function computerChoice(){
     return choices[index];
     
 }
+
+
 console.log("Computer choice: "+ computerChoice());
-let playerChoice = " "'
+let playerChoice = " ";
+
+
 function getHumanChoice(){
-    let choice = prompt("Choose a hand to play for the game", "will you choose rock, paper, or scissors?");
+    let choice = prompt("Choose a hand to play for the game", 
+                        "will you choose rock, paper, or scissors?");
     
-    if(choice === "rock")
+    if(choice === "rock"){
+        return playerChoice = "rock";
+    }
+    else if(choice === "paper"){
+        return playerChoice = "paper";
+    }
+    else if(choice === "scissors"){
+        return playerChoice = "scissors";
+    }
 }
 
 console.log(getHumanChoice());
